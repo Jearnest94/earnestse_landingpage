e5c66e91a8dcd220bbc7480b58352483f15579d7
jearnest94 <jearnest1994@gmail.com>
2024-08-27 01:21:44 +0200

    made the (fake) submit button freak out

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const hoursElement = document.querySelector('.hours');
 const minutesElement = document.querySelector('.minutes');
 const secondsElement = document.querySelector('.seconds');
 const emailformElement = document.querySelector('.emailform');
-const buttonElement = document.getElementById('dabutton');
+const buttonElement = document.querySelector('form button[type="button"]');
+let coinflip_small = Math.round(Math.random()) * 2 - 1;
+let coinflip_medium = Math.round(Math.random()) * 2 - 1;
+let coinflip_large = Math.round(Math.random()) * 2 - 1;
 
 let targetValues = {
     days: 0,
@@ -19,7 +22,7 @@ function updateTimer() {
     targetValues = {
         days: Math.floor(13 + (Math.random() * 4)),
         hours: Math.floor(12 + (Math.random() * 10)),
-        minutes: Math.floor(1 + (Math.random() * 69)),
+        minutes: Math.floor(21 + (Math.random() * 33)),
         seconds: Math.floor(1 + (Math.random() * 69))
     };
 
@@ -34,80 +37,66 @@ function updateTimer() {
 function getCursor(event) {
     let cursor_x = event.clientX;
     let cursor_y = event.clientY;
-    const emailformElementRect = emailformElement.getBoundingClientRect();
-    let button;
+    const buttonElementRect = buttonElement.getBoundingClientRect();
 
     const xThresholdBig = 250; // Adjust this value as needed
     const yThresholdBig = 250; // Adjust this value as needed
-    const xThreshold = 100; // Adjust this value as needed
-    const yThreshold = 100; // Adjust this value as needed
-    const xThresholdSmall = 50; // Adjust this value as needed
-    const yThresholdSmall = 50; // Adjust this value as needed
+    const xThreshold = 160; // Adjust this value as needed
+    const yThreshold = 160; // Adjust this value as needed
+    const xThresholdSmall = 45; // Adjust this value as needed
+    const yThresholdSmall = 45; // Adjust this value as needed
+    const xThresholdTiny = 14;
+    const yThresholdTiny = 14;
 
-
-
-    // if (cursor_x >= emailformElementRect.left - xThresholdBig &&
-    //     cursor_x <= emailformElementRect.right + xThresholdBig &&
-    //     cursor_y >= emailformElementRect.top - yThresholdBig &&
-    //     cursor_y <= emailformElementRect.bottom + yThresholdBig) {
-    //         console.log("NEAR email form!");
-
-    //     if (cursor_x >= emailformElementRect.left - xThreshold &&
-    //         cursor_x <= emailformElementRect.right + xThreshold &&
-    //         cursor_y >= emailformElementRect.top - yThreshold &&
-    //         cursor_y <= emailformElementRect.bottom + yThreshold) {
-    //         console.log("Cursor is veryvery near the email form!");
-    //     } 
-    // } else {
-    //     console.log("ELSE!");
-    // }
-
-
-    if (cursor_x >= emailformElementRect.left - xThresholdSmall &&
-        cursor_x <= emailformElementRect.right + xThresholdSmall &&
-        cursor_y >= emailformElementRect.top - xThresholdSmall &&
-        cursor_y <= emailformElementRect.bottom + xThresholdSmall) {
-        console.log("EXTREMELY VERY NEAR email form!");
-    } else if (cursor_x >= emailformElementRect.left - xThreshold &&
-        cursor_x <= emailformElementRect.right + xThreshold &&
-        cursor_y >= emailformElementRect.top - yThreshold &&
-        cursor_y <= emailformElementRect.bottom + yThreshold) {
-        console.log("VERY NEAR email form!");
-    } else if (cursor_x >= emailformElementRect.left - xThresholdBig &&
-        cursor_x <= emailformElementRect.right + xThresholdBig &&
-        cursor_y >= emailformElementRect.top - yThresholdBig &&
-        cursor_y <= emailformElementRect.bottom + yThresholdBig) {
-        console.log("NEAR email form!");
-
+    if (cursor_x >= buttonElementRect.left - xThresholdTiny &&
+        cursor_x <= buttonElementRect.right + xThresholdTiny &&
+        cursor_y >= buttonElementRect.top - yThresholdTiny &&
+        cursor_y <= buttonElementRect.bottom + yThresholdTiny) {
+        console.log("0");
+        const button = document.querySelector('form button[type="button"]'); // Select the button
+        button.classList.remove('cursor_far'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_close'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_very_close'); // Remove the 'near-hover' class
+        button.classList.add('cursor_ultra_close');
+    } else if (cursor_x >= buttonElementRect.left - xThresholdSmall &&
+        cursor_x <= buttonElementRect.right + xThresholdSmall &&
+        cursor_y >= buttonElementRect.top - yThresholdSmall &&
+        cursor_y <= buttonElementRect.bottom + yThresholdSmall) {
+        console.log("1");
+        const button = document.querySelector('form button[type="button"]'); // Select the button
+        button.classList.remove('cursor_close'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_very_close'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_ultra_close'); // Remove the 'near-hover' class
+        button.classList.add('cursor_very_close');
+    } else if (cursor_x >= buttonElementRect.left - xThreshold &&
+        cursor_x <= buttonElementRect.right + xThreshold &&
+        cursor_y >= buttonElementRect.top - yThreshold &&
+        cursor_y <= buttonElementRect.bottom + yThreshold) {
+        console.log("2");
+        const button = document.querySelector('form button[type="button"]'); // Select the button
+        button.classList.remove('cursor_very_close'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_far'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_ultra_close'); // Remove the 'near-hover' class
+        button.classList.add('cursor_close');
+    } else if (cursor_x >= buttonElementRect.left - xThresholdBig &&
+        cursor_x <= buttonElementRect.right + xThresholdBig &&
+        cursor_y >= buttonElementRect.top - yThresholdBig &&
+        cursor_y <= buttonElementRect.bottom + yThresholdBig) {
+        console.log("3");
+        const button = document.querySelector('form button[type="button"]'); // Select the button
+        button.classList.remove('cursor_far'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_close'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_ultra_close'); // Remove the 'near-hover' class
+        button.classList.add('cursor_far');
     } else {
-        console.log("ELSE!");
+        const button = document.querySelector('form button[type="button"]'); // Select the button
+        button.classList.remove('cursor_far'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_close'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_very_close'); // Remove the 'near-hover' class
+        button.classList.remove('cursor_ultra_close'); // Remove the 'near-hover' class
+
     }
 }
-
-
-//         button = document.getElementById("dabutton");
-//         console.log(button.classList);
-
-//         button.classList.add('wobbleshake'); // Add the 'near-hover' class
-
-//         //button.style.backgroundColor = '#fffa76'; // Change the button's color to yellow
-//     } else if (
-//         cursor_x >= emailformElementRect.left - xThreshold &&
-//         cursor_x <= emailformElementRect.right + xThreshold &&
-//         cursor_y >= emailformElementRect.top - yThreshold &&
-//         cursor_y <= emailformElementRect.bottom + yThreshold
-//     )  {
-//         button = document.getElementById("dabutton");
-//         console.log(button.classList);
-//         button.classList.remove('wobbleshake'); // Remove the 'near-hover' class
-
-//     } else {
-//         button = document.getElementById("dabutton");
-//         console.log(button.classList);
-//         console.log('else');
-//     }
-
-// }
 
 // Initial update
 updateTimer();
